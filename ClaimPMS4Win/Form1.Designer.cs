46354139bac9a0a8321987ff4086fe485f169b76
Tommy Mikkelsen <tm@orstrand.dk>
2022-08-28 21:23:37 +0200

    Init

---
+++ ClaimPMS4Win/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ClaimPMS4Win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.tbUsrName = new System.Windows.Forms.TextBox();
+            this.lblUsrName = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.lbl2Fa = new System.Windows.Forms.Label();
+            this.tb2FA = new System.Windows.Forms.TextBox();
+            this.btnClaim = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Location = new System.Drawing.Point(12, 12);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.ReadOnly = true;
+            this.rtbOut.Size = new System.Drawing.Size(427, 426);
+            this.rtbOut.TabIndex = 1;
+            this.rtbOut.Text = "";
+            // 
+            // tbUsrName
+            // 
+            this.tbUsrName.Location = new System.Drawing.Point(458, 28);
+            this.tbUsrName.Name = "tbUsrName";
+            this.tbUsrName.Size = new System.Drawing.Size(289, 20);
+            this.tbUsrName.TabIndex = 2;
+            // 
+            // lblUsrName
+            // 
+            this.lblUsrName.AutoSize = true;
+            this.lblUsrName.Location = new System.Drawing.Point(455, 12);
+            this.lblUsrName.Name = "lblUsrName";
+            this.lblUsrName.Size = new System.Drawing.Size(63, 13);
+            this.lblUsrName.TabIndex = 3;
+            this.lblUsrName.Text = "User Name:";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(458, 55);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(96, 13);
+            this.lblPwd.TabIndex = 4;
+            this.lblPwd.Text = "Plex TV Password:";
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(461, 72);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.PasswordChar = '*';
+            this.tbPwd.Size = new System.Drawing.Size(286, 20);
+            this.tbPwd.TabIndex = 5;
+            // 
+            // lbl2Fa
+            // 
+            this.lbl2Fa.AutoSize = true;
+            this.lbl2Fa.Location = new System.Drawing.Point(461, 99);
+            this.lbl2Fa.Name = "lbl2Fa";
+            this.lbl2Fa.Size = new System.Drawing.Size(104, 13);
+            this.lbl2Fa.TabIndex = 6;
+            this.lbl2Fa.Text = "2FA code (Optional):";
+            // 
+            // tb2FA
+            // 
+            this.tb2FA.Location = new System.Drawing.Point(464, 116);
+            this.tb2FA.Name = "tb2FA";
+            this.tb2FA.Size = new System.Drawing.Size(283, 20);
+            this.tb2FA.TabIndex = 7;
+            // 
+            // btnClaim
+            // 
+            this.btnClaim.Location = new System.Drawing.Point(550, 166);
+            this.btnClaim.Name = "btnClaim";
+            this.btnClaim.Size = new System.Drawing.Size(75, 23);
+            this.btnClaim.TabIndex = 8;
+            this.btnClaim.Text = "Claim";
+            this.btnClaim.UseVisualStyleBackColor = true;
+            this.btnClaim.Click += new System.EventHandler(this.btnClaim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClaim);
+            this.Controls.Add(this.tb2FA);
+            this.Controls.Add(this.lbl2Fa);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblUsrName);
+            this.Controls.Add(this.tbUsrName);
+            this.Controls.Add(this.rtbOut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.TextBox tbUsrName;
+        private System.Windows.Forms.Label lblUsrName;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.Label lbl2Fa;
+        private System.Windows.Forms.TextBox tb2FA;
+        private System.Windows.Forms.Button btnClaim;
+    }
+}
+
---
+++ ClaimPMS4Win/Form1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+
+
+
+namespace ClaimPMS4Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static readonly HttpClient client = new HttpClient();
+
+        private string FriendlyName;
+        private string PlexOnlineMail;
+        private string PlexOnlineToken;
+        private string PlexOnlineUsername;
+        private string PlexOnlineHome;
+
+        private void addLog( string text)
+        {
+            this.rtbOut.AppendText(text + "\r\n");
+        }
+
+        private Boolean pmsPresent()
+        {
+            RegistryKey pmsKeys = Registry.CurrentUser.OpenSubKey(@"Software\Plex, Inc.\Plex Media Server", true);
+            if (pmsKeys != null)
+            {
+                FriendlyName = (String)pmsKeys.GetValue("FriendlyName");
+                PlexOnlineMail = (String)pmsKeys.GetValue("PlexOnlineMail");
+                PlexOnlineToken = (String)pmsKeys.GetValue("PlexOnlineToken");
+                PlexOnlineUsername = (String)pmsKeys.GetValue("PlexOnlineUsername");
+                this.tbUsrName.Text = PlexOnlineUsername;
+                PlexOnlineHome = pmsKeys.GetValue("PlexOnlineHome").ToString();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async Task<bool> loginPlexTVAsync()
+        {
+            string url;
+            if (this.tb2FA.Text != "")
+            {
+                url = String.Format("https://plex.tv/api/v2/users/signin?login={0}&password={1}&verificationCode={2}", Uri.EscapeDataString(this.tbUsrName.Text), Uri.EscapeDataString(this.tbPwd.Text), Uri.EscapeDataString(this.tb2FA.Text));
+            } else
+            {
+                url = String.Format("https://plex.tv/api/v2/users/signin?login={0}&password={1}", Uri.EscapeDataString(this.tbUsrName.Text), Uri.EscapeDataString(this.tbPwd.Text));
+            }
+
+            var response = await client.PostAsync(url, null);
+            var statusCode = response.StatusCode;
+            if ((int)statusCode == 1029)
+            {
+                this.addLog("2FA token needed");
+
+
+            }
+            var responseMsg = await response.Content.ReadAsStringAsync();
+            if (statusCode == System.Net.HttpStatusCode.OK)
+            {
+                this.addLog("Authenticated towards plex.tv okay");
+            } else
+            {
+                this.addLog("We encountered an error!");
+
+
+            }
+
+
+
+
+
+            //      axios({
+            //      method: 'POST',
+            //url: url,
+            //headers: wtutils.PMSHeader
+            //          })
+
+
+
+            return false;
+        }
+
+        private void setDefaultHeaders()
+        {
+            // Add some default headers for Web Requests
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("X-Plex-Device", "Windows");
+            String firstMacAddress = NetworkInterface.GetAllNetworkInterfaces().Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback).Select(nic => nic.GetPhysicalAddress().ToString()).FirstOrDefault();
+            client.DefaultRequestHeaders.Add("X-Plex-Client-Identifier", firstMacAddress);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            string version = assembly.GetName().Version.ToString();
+            // Also set as default header towards plex.tv
+            client.DefaultRequestHeaders.Add("X-Plex-Version", version);
+            string name = assembly.GetName().Name;
+            // Also set as default header towards plex.tv
+            client.DefaultRequestHeaders.Add("X-Plex-Product", name);
+            this.setDefaultHeaders();
+            this.Text = name + " v" + version;
+            this.addLog("Velcome to " + name);
+            this.addLog("");
+            this.addLog("Note: When we say PMS, we mean the Plex Media Server");
+            this.addLog("");
+            this.addLog("This 3rd. Party tool will assist you in (Re-) Claiming a PMS");
+            this.addLog("");
+            this.addLog("This tool MUST be run on the PMS itself, and as the user that normally runs the PMS");
+            this.addLog("");
+            this.addLog("");
+            if (pmsPresent())
+            {
+                this.addLog("We found an installed PMS, so good so far");
+                this.addLog("");
+                this.addLog("PMS Name detected as: " + FriendlyName);
+                if (this.PlexOnlineUsername != null)
+                {
+                    this.btnClaim.Text = "Re-claim";
+                    this.addLog("PMS detected as been claimed");
+                } else
+                {
+                    this.addLog("PMS detected as not claimed");
+                }
+                this.addLog("");
+                this.addLog("Enter credentials to the right, and press the button");
+            } else
+            {
+                this.addLog("No PMS server found");
+                this.addLog("You have to run this on the actual PMS server!");
+                this.addLog("and with the correct user!");
+                this.btnClaim.Enabled = false;
+            }
+        }
+
+        private void btnClaim_Click(object sender, EventArgs e)
+        {
+            _ = this.loginPlexTVAsync();
+        }
+    }
+}
